c0ce184285e938f6c29dff909b8de86513ed0c5b
sdpy5 <73287400+sdpy5@users.noreply.github.com>
2020-11-09 13:32:20 +0530

    Update dictionary_inside_dictionary.py

--- dictionary_inside_dictionary.py
+++ dictionary_inside_dictionary.py
@@ -18,3 +18,17 @@ for usernames,user_details in users.items():
     location = user_details['location']
     print('\tusers full name is >>>> ' + fullname.title())
     print('\tusers location is >>>> ' + location.title())
+
+    
+    #output
+    
+the user name is >>> Sangith Dharshan
+        users full name is >>>> Sangithdharshan
+        users location is >>>> Bangalore
+the user name is >>> Shravanya
+        users full name is >>>> Shravanyasunildas
+        users location is >>>> Bagalgunte
+the user name is >>> Sushagh
+        users full name is >>>> Sushaghsomarajan
+        users location is >>>> Kannur
+    
